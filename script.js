e86dff13ff92286db67da31447c81716ca1f16cd
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-06-29 17:34:48 -0500

    Added pseudo-coding to create a list of what to do

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // Prompt question how many characters in password
+  // var for characters
+
+  //  What type of characters
+  // special character, numbers, uppercase, and lowercase
+  // window.confirm
+
+  // Use answers to make password
+  // vars that include all possible characters
+  // separate vars for each type, arrays
+  // one var for special character with arrays
+  // EX: var upperLetters = ["A", "B", "C", "D"]
+
+  // take all true arrays and concat into new array - loop
+  // choose random chars from array based on user input
+  // var push into new array
+  // var password = turn array into string .toString()
+
+  // return password
+
+
+}
