862d665dd25ae2b072898b08e3e4ace0dceb2213
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-06-30 22:42:56 -0500

    fixed confirm window to properly run and connect to if statements. if statements added to choose characters and combine into new array, added alert if no option is chosen

--- script.js
+++ script.js
@@ -19,55 +19,70 @@ function generatePassword() {
   // store var for characters
   
   var userCharacters = window.prompt("How many characters do you want your password to be?");
-  console.log(userCharacters);
   
   if (userCharacters < 8 || userCharacters > 128) {
     window.alert("Your password must be at least 8 characters long and no more than 128.");
     return;
   };
   
-  window.confirm("Do you want to use special characters?");
-  window.confirm("Do you want to use numbers?");
-  window.confirm("Do you want to use uppercase letters?");
-  window.confirm("Do you want to use lowercase letters?");
-  
-  var specialCharacters = ["!", "U+0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "U+005C", "]", "^", "_", "`", "{", "|", "}","~"];
-
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  
-  
-  // if (userCharacters >=8  <=128) {
-    
-    // }
-
-
-  //  What type of characters
+  // What type of characters do you want
   // special character, numbers, uppercase, and lowercase
   // window.confirm
-
-
-
-  // Use answers to make password
-  // vars that include all possible characters
-  // separate vars for each type, arrays
-  // EX: var upperLetters = ["A", "B", "C", "D"]
   
-  if (specialCharacters && numbers && upperCase && lowerCase) {
-    var allTrue = specialCharacters.concat(numbers, upperCase, lowerCase);
-  } else if (specialCharacters && numbers && upperCase) {
-    var specNumUpper = specialCharacters.concat(numbers, upperCase);
-  } else if (specialCharacters && numbers) {
-    var specNum = specialCharacters.concat(numbers);
-  } else if (specNum && lowerCase) {
-    var specNumLower = specNum.concat(lowerCase);
-  }
-  console.log (allTrue)
+  var getSpecCharacters = window.confirm("Do you want to use special characters?");
+  var getNumbers = window.confirm("Do you want to use numbers?");
+  var getUpper = window.confirm("Do you want to use uppercase letters?");
+  var getLower = window.confirm("Do you want to use lowercase letters?");
+  
+  
+  // if (!getSpecCharacters && !getNumbers && !getUpper && !getLower) {
+    //   window.alert("Your must choose at least one type of character to create a password. Please try again.");
+  //   return;
+  // }
+  
+  // Use answers to make password
+  //  vars that include all possible characters
+  //  separate vars for each type, arrays
+  //  EX: var upperLetters = ["A", "B", "C", "D"]
+  var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}","~"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+  // take all true arrays and concat into new array
 
-  // take all true arrays and concat into new array - loop
+  var chosenChars = "";
+  
+  if (getSpecCharacters) {
+    chosenChars = specialCharacters.concat(chosenChars);
+  };
+
+  if (getNumbers) {
+    chosenChars = numbers.concat(chosenChars);
+  };
+
+  if (getUpper) {
+    chosenChars = upperCase.concat(chosenChars);
+  };
+
+  if (getLower) {
+    chosenChars = lowerCase.concat(chosenChars);
+    // spec characters and numbers
+  };
+
+  if (!getSpecCharacters && !getNumbers && !getUpper && !getLower) {
+    window.alert("Your must choose at least one type of character to create a password. Please try again.");
+      return;
+  };
+  
+  for (i = 0; i < userCharacters; i++) {
+    var passwordChars = chosenChars[Math.floor(Math.random() * chosenChars.length)];
+    password = chosenChars.concat(passwordChars);
+  };
+
+  console.log (passwordChars);
+
+  // Create for loop
   // choose random chars from array based on user input
   // var push into new array
   // var password = turn array into string .toString()
